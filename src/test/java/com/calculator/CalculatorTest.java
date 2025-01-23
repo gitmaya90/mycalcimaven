af52cd3ba926cdbc983e617c93e102068039b412
gitmaya90 <design.maxdesk@gmail.com>
2025-01-23 16:18:07 +0530

    first commit

---
+++ src/test/java/com/calculator/CalculatorTest.java
@@ -0,0 +1,41 @@
+package com.calculator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        assertEquals(5,calculator.add(3,2));
+    }
+
+    @Test
+    public void testSubtract() {
+        Calculator calculator = new Calculator();
+        assertEquals(1, calculator.subtract(3, 2));
+    }
+
+    @Test
+    public void testMultiply() {
+        Calculator calculator = new Calculator();
+        assertEquals(6, calculator.multiply(2, 3));
+    }
+
+    @Test
+    public void testDivide() {
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.divide(6, 3));
+    }
+
+    @Test
+    public void testDivideByZero() {
+        Calculator calculator = new Calculator();
+        try {
+            calculator.divide(1, 0);
+        } catch (IllegalArgumentException e) {
+            assertEquals("Division by zero is not allowed.", e.getMessage());
+        }
+    }
+}
