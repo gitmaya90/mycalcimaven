b95c163407306946eb39399657f849f680386d3c
Mayank Kulhare <design.maxdesk@gmail.com>
2025-03-03 08:44:49 +0000

    Update CalculatorTest.java

--- src/test/java/com/calculator/CalculatorTest.java
+++ src/test/java/com/calculator/CalculatorTest.java
@@ -5,31 +5,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
-    @Test
+
     public void testAdd() {
         Calculator calculator = new Calculator();
         assertEquals(5,calculator.add(3,2));
     }
 
-    @Test
+  
     public void testSubtract() {
         Calculator calculator = new Calculator();
         assertEquals(1, calculator.subtract(3, 2));
     }
 
-    @Test
     public void testMultiply() {
         Calculator calculator = new Calculator();
         assertEquals(6, calculator.multiply(2, 3));
     }
 
-    @Test
     public void testDivide() {
         Calculator calculator = new Calculator();
         assertEquals(2, calculator.divide(6, 3));
     }
 
-    @Test
     public void testDivideByZero() {
         Calculator calculator = new Calculator();
         try {
